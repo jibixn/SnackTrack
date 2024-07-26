43670842853e12b37a9bc580a3fd4f5186d2471c
Anantha Krishnan <jananthak@gmail.com>
2024-07-26 12:13:26 +0530

    fixxxxxxxx

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -11,14 +11,11 @@ const User= require('../models/User');
 authRouter.post("/api/Signup",async (req,res) =>{
     try{
         const {name,img,username,password,department,role} = req.body;
-
-
         const existingUser=await User.findOne({username});
         if(existingUser){
             return res.status(400)
             .json({message:"User Already Exists"});
         }
-
         const hashed = await bcryptjs.hash(password,8);
         let user = new User({
             name,
@@ -36,75 +33,6 @@ authRouter.post("/api/Signup",async (req,res) =>{
     }
 
 });
-
-
-// authRouter.post("/api/Signin",async (req,res) =>{
-//     try{
-//         const {username,password} = req.body;
-
-//         const user =await User.findOne({username});
-
-//         if(!user){
-//             return res.status(400)
-//             .json({message:"User Does not Exist"});
-//         }
-
-//         const match =await bcryptjs.compare(password , user.password);
-
-//         if(!match){
-//             return res.status(400)
-//             .json({message:"Incorrect Password"})
-//         }
-
-//         const token = jwt.sign({id: user._id },"passwordKey");
-        
-//         res.json({token,...user._doc});
-
-       
-
-
-
-
-//     }catch(e){
-//         res.status(500).json({error:e.message});
-
-//     }
-
-// });
-
-// authRouter.post("/api/EmpSignin", async (req, res) => {
-//     try {
-//       const { username, password } = req.body;
-//       const user = await User.findOne({ username });
-  
-//       if (!user) {
-//         return res.status(400).json({ message: "User Does not Exist" });
-//       }
-  
-//       const match = await bcryptjs.compare(password, user.password);
-//       if (!match) {
-//         return res.status(400).json({ message: "Incorrect Password" });
-//       }
-  
-//       if (user.role !== 'employee' && user.role !== 'manager') {
-//         return res.status(403).json({ message: "Access denied. Not a faculty or manager." });
-//       }
-  
-//       const token = jwt.sign({ id: user._id, role: user.role }, "passwordKey");
-  
-//       const userResponse = user.toObject();
-//       delete userResponse.password;
-  
-//       res.json({ token, user: userResponse, role: user.role });
-  
-//     } catch (e) {
-//       res.status(500).json({ error: e.message });
-//     }
-//   });
-
-// module.exports = authRouter;
-
-
 
 authRouter.post("/api/Signin",async (req,res) =>{
     try{
